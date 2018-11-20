ae1b2f0b768b43e573b3e8e76580b3af008cc079
Yeonwoo <yeonwookang93@gmail.com>
2018-11-21 04:05:26 +0900

    서비스가 꺼져있을 때, 일반 저장 방식을 따르도록 수정

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
-var url;
-var mime;
-var file_type;
-var person;
-var confidence;
-
+var url; // 다운로드 url
+var mime; // 파일 종류
+var file_type; // 파일 종류 => 파일 확장자
+var filename = "unknown"; // 인물 이름
+var confidence = 0.0; // 신뢰도
 var toggle = false; // 토글 버튼 초기 상태
 
 // 다운로드시에 false
@@ -20,9 +19,8 @@ whale.storage.onChanged.addListener(function (changes, namespace) {
     }
 });
 
+// 다운로드 클릭시
 whale.downloads.onCreated.addListener(evt => {
-
-	// 다운로드 클릭시
     // alert('다운로드가 시작되었습니다.');
 
     // download url 가져오기
@@ -31,26 +29,26 @@ whale.downloads.onCreated.addListener(evt => {
 
     // mime을 확장자로 변환
     switch(mime) {
-	case 'image/gif':
-		file_type = ".gif";
-	break;
-	case 'image/png':
-		file_type = ".png";
-	break;
-	case 'image/jpeg':
-		file_type = ".jpg";
-	break;
-	case 'image/bmp':
-		file_type = ".bmp";
-	break;
-	case 'image/webp':
-		file_type = ".webp";
-	break;
-	case 'image/vnd.microsoft.icon':
-		file_type = ".ico";
-	break;
+    	case 'image/gif':
+    		file_type = ".gif";
+    	break;
+    	case 'image/png':
+    		file_type = ".png";
+    	break;
+    	case 'image/jpeg':
+    		file_type = ".jpg";
+    	break;
+    	case 'image/bmp':
+    		file_type = ".bmp";
+    	break;
+    	case 'image/webp':
+    		file_type = ".webp";
+    	break;
+    	case 'image/vnd.microsoft.icon':
+    		file_type = ".ico";
+    	break;
 
-	default: break;
+    	default: break;
 	}
 
 });
@@ -74,32 +72,32 @@ whale.downloads.onDeterminingFilename.addListener(function(item, suggest) {
 
 	    	// person 변수에 인물값 저장, confidence에 정확도 저장
 	    	// CFR API 호출
-	    	confidence = 0.00; // 정확도 
-	    	// 인물을 찾은 경우 
+	    	confidence = 0.00; // 정확도
+	    	// 인물을 찾은 경우
 	    	if(confidence >= 80.0) {
-	    		
-	    		// 파일이름을 인물로 
+
+	    		// 파일이름을 인물로
 
 	    	} else { // 인물을 찾지 못한 경우
-	    	
-		    	// 'unknown'이라는 이름으로 이미지 저장
+
+		    	// 'unknown' 폴더에 'unknown'이라는 이름으로 이미지 저장
 		    	filename = "unknown";
 	    	}
-	     
+
 	    	// alert('이미지 파일입니다.');
 	    	// 저장방식: downlonad/인물이름/인물이름.jpg
-			suggest({filename:  filename + "/" + filename + file_type});
+			  suggest({filename:  filename + "/" + filename + file_type});
 	    	break;
 
-	    	default: // 이미지 파일이 아닌 경우 
+	    	default: // 이미지 파일이 아닌 경우
 	    	//alert('기타 파일입니다.');
-	    	alert(item.filename);
+	    	//alert(item.filename);
 	    	suggest({filename: item.filename}); // 원본 이름
 	    	break;
 	    }
 
-   } else {
-   		alert(item.filename);
+   } else { // 서비스가 꺼져있는 경우
+   		//alert(item.filename);
 	    suggest({filename: item.filename}); // 원본 이름
    }
 });
