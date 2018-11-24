44b664e3146b96b1f49d4718791b14304b459c49
hello2jolee <jioun.lee.dev@gmail.com>
2018-11-25 00:08:33 +0900

    토글 오류 수정

--- sidebar/toggle.js
+++ sidebar/toggle.js
@@ -2,14 +2,13 @@ $(function () {
 
   // toggle 버튼과 스토리지 상의 toggle 정보 동기화
     whale.storage.sync.get(['toggle'], function(result) {
-        if(result.toggle) $('.checkbox[data-type=toggle]').click();
+        if(result.toggle){ $('.checkbox[data-type=toggle]').click();}
     });
 
     // toggle 버튼이 클릭 될 때
     $('.checkbox[data-type=toggle]').on('click', function () {
         var color = $('#lever').css('background-color'); // 레버의 색상으로 판별
-        //alert(color);
-        if(color!=='rgba(0, 0, 0, 0.38)') { //눌러지는 타이밍 기준 배경색이 회색이 아니면
+        if(color!=='rgb(204,204,204)') { //눌러지는 타이밍 기준 배경색이 회색이 아니면
             whale.storage.sync.set({'toggle': false}); // 서비스 끄기
         }else {
             whale.storage.sync.set({'toggle': true}); // 서비스 켜기
