ec31f4567b0b46dd87e551bc651bd2e9d26a002b
SUJEONG <39018715+sujeong0903@users.noreply.github.com>
2018-12-25 20:03:14 +0900

    Update binaryajax.js

--- binaryajax.js
+++ binaryajax.js
@@ -1,3 +1,9 @@
+/*
+ * Binary Ajax 0.1.5
+ * Copyright (c) 2008 Jacob Seidelin, cupboy@gmail.com, http://blog.nihilogic.dk/
+ * MIT License [http://www.opensource.org/licenses/mit-license.php]
+ */
+
 var BinaryFile = function(strData, iDataOffset, iDataLength) {
     var data = strData;
     var dataOffset = iDataOffset || 0;
